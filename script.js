2e960387e1f9dfdc077d785c4209d384b7ee597f
BillRmz <bilyalon@gmail.com>
2023-03-21 16:55:30 -0600

    some changes

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+//Element selection 
+
 const gameboard = document.querySelector('.gameboard');
+const info = document.querySelector('#player');
 
-gameboard.addEventListener('click', TakeTurn);
+//Listeners 
+gameboard.addEventListener('click', takeTurn);
+
+function takeTurn(e){
+    const item = e.target
+    let i = item.id
+   
+    info.textContent = 'o'
+    position = parseInt(i)
+
+    let player =  'X'
+    let gameOver = false
+    
+    while(gameOver != true){
+        takeTurn(player)
+        gameResult = check_game_over();
+        if (gameResult == 'win'){
+            console.log(`${player} Wins`)
+            PrintBoard()
+            gameOver = true
+        }else if (gameResult == 'tie'){
+            console.log('Tie')
+            gameOver = true
+        }else{
+            if(player === 'X'){
+                player = 'O'
+                PrintBoard()
+            }else{
+                player = 'X'
+                PrintBoard()
+            }
+        }
+    }
 
 
-function TakeTurn(e){
- const item = e.target
- let i = item.id
-
- console.log({item, i})
-
- 
- 
 }
